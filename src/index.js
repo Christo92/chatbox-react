9309d4ce2cbcc836282b8bece4facb5dd254e3d2
Christopher <christopher.loubassou@hotmail.fr>
2018-04-01 13:55:08 +0200

    Creation of Connexion component

--- src/index.js
+++ src/index.js
@@ -1,8 +1,22 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+// Import css
+import './index.css';
+
+class Connexion extends Component {
+    render() {
+        return (
+            <div className="connexionBox">
+                <form className="connexion">
+                    <input type="text" placeholder="Pseudo" required/>
+                    <button type="sumbit">Valider</button>
+                </form>
+            </div>
+        )
+    };
+}
+
+ReactDOM.render(
+    <Connexion/>, document.getElementById('root')
+);
