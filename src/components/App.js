59e8a6501769a5b56ef6c7e4fb10c58d55de8b8d
Christopher <christopher.loubassou@hotmail.fr>
2018-04-02 21:52:19 +0200

    Improvement Form

--- src/components/App.js
+++ src/components/App.js
@@ -6,15 +6,38 @@ import Message from './Message';
 
 
 class App extends Component {
+
+    state = {
+        messages: {},
+    }
+
+    addMessage = message => {
+        // Copier le state
+        const messages = {...this.state.messages};
+
+        // On ajoute le message avec une clé timestamp
+        const timestamp = Date.now();
+        messages[`message-${timestamp}`] = message;
+
+        // Mettre à jour notre State
+        this.setState({ messages });
+    }
+
     render() {
         return(
             <div className="box">
                 <div>
                     <div className="messages">
-                        <Message pseudo="Chris"/>
+                        <Message 
+                            pseudo={ this.props.match.params.pseudo }
+                        />
                     </div>
 
-                    <Formulaire/>
+                    <Formulaire 
+                        addMessage={ this.addMessage } 
+                        pseudo={ this.props.match.params.pseudo }
+                        length="140"
+                    />
                 </div>
             </div>
         )
--- src/components/Connexion.js
+++ src/components/Connexion.js
@@ -1,16 +1,35 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 
 class Connexion extends Component {
+
+    goToChat = (event) => {
+        event.preventDefault();
+        const pseudo = this.pseudoInput.value;
+
+        // Changer l'url
+        this.props.history.push(`/pseudo/${pseudo}`)
+    };
+
     render() {
         return (
-            <div className="connexionBox">
+            <div className="connexionBox" onSubmit={e => this.goToChat(e)}>
                 <form className="connexion">
-                    <input type="text" placeholder="Pseudo" required/>
+                    <input 
+                        type="text" 
+                        placeholder="Pseudo" 
+                        required 
+                        ref={input => this.pseudoInput = input}
+                    />
                     <button type="sumbit">Valider</button>
                 </form>
             </div>
         )
     };
+
+    static contextTypes = {
+        router: PropTypes.object
+    }
 }
 
 export default Connexion;
